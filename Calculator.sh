453bdcbef510a54ad070a505c17c42197794857b
VladB91 <56296271+VladB91@users.noreply.github.com>
2022-03-10 12:57:21 +0200

    Create Calculator.sh

---
+++ Calculator.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+ 
+# Take user Input
+echo "Enter Two numbers : "
+read numberOne
+read numberTwo
+ 
+# Input type of operation
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch
+ 
+# Switch Case to perform
+# calculator operations
+case $ch in
+  1)res=`echo $numberOne + $numberTwo | bc`
+  ;;
+  2)res=`echo $numberOne - $numberTwo | bc`
+  ;;
+  3)res=`echo $numberOne \* $numberTwo | bc`
+  ;;
+  4)res=`echo "scale=2; $numberOne / $numberTwo" | bc`
+  ;;
+esac
+echo "Result : $res"
