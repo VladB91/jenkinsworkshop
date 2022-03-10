d13bec04f97385d724f388817f417258b58f279d
VladB91 <56296271+VladB91@users.noreply.github.com>
2022-03-10 13:46:10 +0200

    Update Calculator.sh

--- Calculator.sh
+++ Calculator.sh
@@ -7,19 +7,27 @@ echo "The two numbers selected in build are: $numberOne and $numberTwo"
 echo "Calculator operation selected: $operation"
  
 # calculator operations
-if [[ $operation -eq "Addition" ]]
-then
-  res=`echo $numberOne + $numberTwo | bc`
-if [[ $operation -eq "Subtraction" ]]
-then
-  res=`echo $numberOne - $numberTwo | bc`
-if [[ $operation -eq "Multiplication" ]]
-then
-  res=`echo $numberOne \* $numberTwo | bc`
-if [[ $operation -eq "Division" ]]
-then
-  res=`echo "scale=2; $numberOne / $numberTwo" | bc`
-else
-  echo "Operation not valid."
-fi
+res=$(
+  case "$operation" in
+    ("Addition") `echo $numberOne + $numberTwo | bc` ;;
+    ("Subtraction") `echo $numberOne - $numberTwo | bc` ;;
+    ("Multiplication") `echo $numberOne \* $numberTwo | bc` ;;
+    ("Division") `echo "scale=2; $numberOne / $numberTwo" | bc` ;;
+    (*) echo "$operation" ;;
+  esac)
+# if [[ $operation -eq "Addition" ]]
+# then
+#   res=`echo $numberOne + $numberTwo | bc`
+# if [[ $operation -eq "Subtraction" ]]
+# then
+#   res=`echo $numberOne - $numberTwo | bc`
+# if [[ $operation -eq "Multiplication" ]]
+# then
+#   res=`echo $numberOne \* $numberTwo | bc`
+# if [[ $operation -eq "Division" ]]
+# then
+#   res=`echo "scale=2; $numberOne / $numberTwo" | bc`
+# else
+#   echo "Operation not valid."
+# fi
 echo "Result of $operation: $res"
